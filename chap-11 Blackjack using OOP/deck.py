49d560b7ea4e8b7e0846bf7a6e6323276954207b
Sandeep Gogineni <san.gogineni@gmail.com>
2022-01-07 03:06:23 -0500

    Blackjack game initial setup

---
+++ chap-11 Blackjack using OOP/deck.py
@@ -0,0 +1,19 @@
+from card import Card
+import random
+
+class Deck:
+    suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+    ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+    def __init__(self):
+        self.deck_of_cards=[]
+        for suit in self.suits:
+            for rank in self.ranks:
+                self.deck_of_cards.append(Card(suit,rank))
+        
+
+    def shuffle_deck(self):
+        random.shuffle(self.deck_of_cards)
+
+    def deal_one(self):
+        return self.deck_of_cards.pop()
---
+++ chap-11 Blackjack using OOP/player.py
@@ -0,0 +1,7 @@
+from deck import Deck
+from card import Card
+
+class Player:
+    def __init__(self,name):
+        self.name=name
+        self.all_cards = []
